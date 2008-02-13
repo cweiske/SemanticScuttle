8fb33e0ac9b63f4b8047aca3d488c95410cc8f50
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2008-02-13 18:16:08 +0000

    Interface fix: fix links to bookmarks into the users box
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@40 b3834d28-1941-0410-a4f8-b48e95affb8f

--- templates/sidebar.block.users.php
+++ templates/sidebar.block.users.php
@@ -20,7 +20,7 @@ foreach ($lastUsers as $row) {
     echo  '<a href="'.createURL('profile', $row['username']).'">';
     echo $row['username'];
     echo '</a>';
-    echo ' (<a href="'.createURL('bookmarks', $user).'">'.T_('bookmarks').'</a>)';
+    echo ' (<a href="'.createURL('bookmarks', $row['username']).'">'.T_('bookmarks').'</a>)';
     echo '</td></tr>';
 }
 echo '<tr><td><a href="'.createURL('users').'">...</a></td></tr>';
