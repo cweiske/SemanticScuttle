50863e8fee72c0236b8d262e6f95f4ace85961c0
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2008-02-20 15:21:30 +0000

    Interface fix: add About link for logged users
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@50 b3834d28-1941-0410-a4f8-b48e95affb8f

--- templates/toolbar.inc.php
+++ templates/toolbar.inc.php
@@ -13,6 +13,7 @@ if ($userservice->isLoggedOn()) {
 	<li><a href="<?php echo $userservice->getProfileUrl($cUserId, $cUsername); ?>"><?php echo T_('Profile'); ?></a></li>
         <li><a href="<?php echo createURL('bookmarks', $cUsername . '?action=add'); ?>"><?php echo T_('Add a Bookmark'); ?></a></li>
         <li class="access"><?php echo $cUsername?><a href="<?php echo $GLOBALS['root']; ?>?action=logout">(<?php echo T_('Log Out'); ?>)</a></li>
+        <li><a href="<?php echo createURL('about'); ?>"><?php echo T_('About'); ?></a></li>
     </ul>
 
 <?php
