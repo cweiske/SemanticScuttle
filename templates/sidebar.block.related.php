0fef208b7029d05f10d221afbd6e7cf740443b64
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2008-03-12 09:17:13 +0000

    Bug fix: don't look for users in related tags box if not existing
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@66 b3834d28-1941-0410-a4f8-b48e95affb8f

--- templates/sidebar.block.related.php
+++ templates/sidebar.block.related.php
@@ -6,6 +6,9 @@ $logged_on_userid = $userservice->getCurrentUserId();
 if ($logged_on_userid === false) {
     $logged_on_userid = NULL;
 }
+if(strlen($user)==0) {
+    $cat_url = createURL('tags', '%2$s');
+}
 if ($currenttag) {
     $relatedTags = $tagservice->getRelatedTags($currenttag, $userid, $logged_on_userid);
     if (sizeof($relatedTags) > 0) {
