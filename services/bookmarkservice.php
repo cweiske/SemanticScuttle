7884e0e33bf6e9502f0b193b33074e9b62563cc0
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2008-03-13 10:11:26 +0000

    Bug fix: correct counting result for history page
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@78 b3834d28-1941-0410-a4f8-b48e95affb8f

--- services/bookmarkservice.php
+++ services/bookmarkservice.php
@@ -370,7 +370,11 @@ class BookmarkService {
         if (SQL_LAYER == 'mysql4') {
             $totalquery = 'SELECT FOUND_ROWS() AS total';
         } else {
-            $totalquery = 'SELECT COUNT(DISTINCT bAddress) AS total'. $query_2 . $query_3 . $query_4;
+	    if ($hash) {
+                $totalquery = 'SELECT COUNT(*) AS total'. $query_2 . $query_3 . $query_4;
+	    } else {
+		$totalquery = 'SELECT COUNT(DISTINCT bAddress) AS total'. $query_2 . $query_3 . $query_4;
+	    }
         }
 
         if (!($totalresult = & $this->db->sql_query($totalquery)) || (!($row = & $this->db->sql_fetchrow($totalresult)))) {
