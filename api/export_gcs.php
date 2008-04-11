5a25753cfaff42b712f4e9add60519ea6f22146c
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2008-04-11 06:56:09 +0000

    New feature: export URL for Google Custom Search (api/export/gcs)
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@103 b3834d28-1941-0410-a4f8-b48e95affb8f

---
+++ api/export_gcs.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Export for Google Custom Search
+*/
+
+// Force HTTP authentication first!
+require_once('httpauth.inc.php');
+require_once('../header.inc.php');
+
+$bookmarkservice =& ServiceFactory::getServiceInstance('BookmarkService');
+$userservice =& ServiceFactory::getServiceInstance('UserService');
+
+/*
+// Restrict to admins?
+if(!$userservice->isAdmin($userservice->getCurrentUserId())) {
+    die(T_('You are not allowed to do this action (admin access)'));
+}*/
+
+
+
+// Check to see if a tag was specified.
+if (isset($_REQUEST['tag']) && (trim($_REQUEST['tag']) != ''))
+    $tag = trim($_REQUEST['tag']);
+else
+    $tag = NULL;
+
+// Get the posts relevant to the passed-in variables.
+$bookmarks =& $bookmarkservice->getBookmarks(0, NULL, NULL, $tag, NULL, getSortOrder());
+
+$currentuser = $userservice->getCurrentUser();
+$currentusername = $currentuser[$userservice->getFieldName('username')];
+
+// Set up the plain file and output all the posts.
+header('Content-Type: text/plain');
+foreach($bookmarks['bookmarks'] as $row) {
+    echo $row['bAddress']."\n";
+}
+
+
+?>
