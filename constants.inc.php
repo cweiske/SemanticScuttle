074cfd0a8cb5851b1c10fe9e3baf71777326e123
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2008-06-04 08:11:11 +0000

    Bug fix: correct problem with PATH_INFO (Thanks to Montie)
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@135 b3834d28-1941-0410-a4f8-b48e95affb8f

--- constants.inc.php
+++ constants.inc.php
@@ -16,4 +16,10 @@ define('PAGE_BOOKMARKS', "bookmarks");
 // INSTALLATION_ID is based on directory DB and used as prefix (in session and cookie) to prevent mutual login for different installations on the same host server
 define('INSTALLATION_ID', md5($GLOBALS['dbname'].$GLOBALS['tableprefix']));
 
+
+// Correct bug with PATH_INFO (maybe for Apache 1)
+if(strlen($_SERVER["PATH_INFO"])<strlen($_SERVER["ORIG_PATH_INFO"])) {
+    $_SERVER["PATH_INFO"] = $_SERVER["ORIG_PATH_INFO"]; 
+}
+
 ?>
