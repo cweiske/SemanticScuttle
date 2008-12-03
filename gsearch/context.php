3a46b91c787d7e95b03e4407b6cebf19f0ead04e
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2008-12-03 14:34:10 +0000

    Bug fix
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@194 b3834d28-1941-0410-a4f8-b48e95affb8f

--- gsearch/context.php
+++ gsearch/context.php
@@ -1,10 +1,10 @@
 <?php
-if($GLOBALS['enableGoogleCustomSearch']==false) {
+require_once('../header.inc.php');
+
+if($GLOBALS['enableGoogleCustomSearch'] == false) {
     echo "Google Custom Search disabled. You can enable it into the config.inc.php file.";
     die;
 }
-
-require_once('../header.inc.php');
 ?>
 
 <!--?xml version="1.0" encoding="UTF-8" ?-->
