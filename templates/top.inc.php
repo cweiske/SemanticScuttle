fbac7319cf413b8cdf5e7cd1ee0d48070b484c8a
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2009-02-09 10:42:15 +0000

    Interface fix: add &raquo; character to page title.
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@258 b3834d28-1941-0410-a4f8-b48e95affb8f

--- templates/top.inc.php
+++ templates/top.inc.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-<title><?php echo filter($GLOBALS['sitename'] . (isset($pagetitle) ? ': ' . $pagetitle : '')); ?></title>
+<title><?php echo filter($GLOBALS['sitename'] .(isset($pagetitle) ? ' » ' . $pagetitle : '')); ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="icon" type="image/png" href="<?php echo ROOT ?>icon.png" />
 <link rel="stylesheet" type="text/css"
