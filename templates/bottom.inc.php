f8f0b34afa9c2232f12efccbdbd4091317e738a8
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2009-02-10 14:13:02 +0000

    New Feature: add Google Analytics Code in config file [Config file modified: googleAnalyticsCode]
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@267 b3834d28-1941-0410-a4f8-b48e95affb8f

--- templates/bottom.inc.php
+++ templates/bottom.inc.php
@@ -12,5 +12,20 @@ if($GLOBALS['enableWebsiteThumbnails']) {
 
 </div>
 
+
+<?php if(isset($GLOBALS['googleAnalyticsCode']) && $GLOBALS['googleAnalyticsCode']!= ''):?>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("<?php echo $GLOBALS['googleAnalyticsCode']?>");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+<?php endif;?>
+
 </body>
 </html>
