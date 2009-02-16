0d725d32c3793d078f50a96e7be47e647fb47fc1
mensonge <mensonge@b3834d28-1941-0410-a4f8-b48e95affb8f>
2009-02-16 12:02:47 +0000

    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@280 b3834d28-1941-0410-a4f8-b48e95affb8f

--- templates/editprofile.tpl.php
+++ templates/editprofile.tpl.php
@@ -57,7 +57,7 @@ $this->includeTemplate($GLOBALS['top_include']);
 <tr>
     <th align="left"><?php echo T_('Export bookmarks'); ?></th>
     <td>
-	<a href="../api/export/html"><?php echo T_('HTML file (for browsers)')?></a> /
+	<a href="../api/export_html.php"><?php echo T_('HTML file (for browsers)')?></a> /
 	<a href="../api/posts/all"><?php echo T_('XML file (like del.icio.us)')?></a>
     </td>
 </tr>
