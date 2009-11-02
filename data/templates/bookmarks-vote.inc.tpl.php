65b1963d160e0a2fad8a099abf69b799e42f7915
cweiske <cweiske@b3834d28-1941-0410-a4f8-b48e95affb8f>
2009-11-02 09:39:07 +0000

    enable vote changes on voting badge
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@500 b3834d28-1941-0410-a4f8-b48e95affb8f

--- data/templates/bookmarks-vote.inc.tpl.php
+++ data/templates/bookmarks-vote.inc.tpl.php
@@ -21,24 +21,24 @@ if (isset($row['hasVoted']) && !$row['hasVoted']) {
 }
 echo '<span class="' . $classes . '" id="bmv-' . $row['bId'] . '">';
 
-if (isset($row['hasVoted']) && !$row['hasVoted']) {
+if (isset($row['hasVoted']) && $row['vote'] != 1) {
     echo '<a class="vote-for" rel="nofollow" href="'
         . createVoteURL(true, $row['bId']) . '"'
         . ' onclick="javascript:vote(' . $row['bId'] . ',1); return false;"'
         . '>+</a>';
 } else {
-    echo '<span class="vote-for-inactive">+</span>';
+    echo '<span class="vote-for vote-for-inactive">+</span>';
 }
 
 echo '<span class="voting">' . $row['bVoting'] . '</span>';
 
-if (isset($row['hasVoted']) && !$row['hasVoted']) {
+if (isset($row['hasVoted']) && $row['vote'] != -1) {
     echo '<a class="vote-against" rel="nofollow" href="'
         . createVoteURL(false, $row['bId']) . '"'
         . ' onclick="vote(' . $row['bId'] . ',-1); return false;"'
         . '>-</a>';
 } else {
-    echo '<span class="vote-against-inactive">-</span>';
+    echo '<span class="vote-against vote-against-inactive">-</span>';
 }
 echo '</span>';
 ?>
