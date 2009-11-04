e85837c659cb10511b586dcca7032b6cc5b7cea9
cweiske <cweiske@b3834d28-1941-0410-a4f8-b48e95affb8f>
2009-11-04 21:37:31 +0000

    separate script to update translation base file
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@517 b3834d28-1941-0410-a4f8-b48e95affb8f

---
+++ scripts/update-translation-base.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Update gettext translation base file
+ */
+chdir(dirname(dirname(__FILE__)));
+
+//do not include php-gettext or database layer
+passthru(
+    'xgettext -kT_ngettext:1,2 -kT_ -L PHP'
+    . ' -o data/locales/messages.po'
+    . ' src/SemanticScuttle/*.php'
+    . ' src/SemanticScuttle/Model/*.php'
+    . ' src/SemanticScuttle/Service/*.php'
+    . ' data/templates/*.php'
+    . ' www/*.php'
+    . ' www/*/*.php'
+);
+
+?>
