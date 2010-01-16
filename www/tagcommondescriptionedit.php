655f60843c209ec622c17a2663f8861678b35a42
cweiske <cweiske@b3834d28-1941-0410-a4f8-b48e95affb8f>
2010-01-16 11:12:19 +0000

    Coding standards changes, add header docblock
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@595 b3834d28-1941-0410-a4f8-b48e95affb8f

--- www/tagcommondescriptionedit.php
+++ www/tagcommondescriptionedit.php
@@ -1,35 +1,39 @@
 <?php
-/***************************************************************************
- Copyright (C) 2006 - 2007 Scuttle project
- http://sourceforge.net/projects/scuttle/
- http://scuttle.org/
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- ***************************************************************************/
+/**
+ * Edits the common tag description
+ *
+ * SemanticScuttle - your social bookmark manager.
+ *
+ * PHP version 5.
+ *
+ * @category Bookmarking
+ * @package  SemanticScuttle
+ * @author   Benjamin Huynh-Kim-Bang <mensonge@users.sourceforge.net>
+ * @author   Christian Weiske <cweiske@cweiske.de>
+ * @author   Eric Dane <ericdane@users.sourceforge.net>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     http://sourceforge.net/projects/semanticscuttle
+ */
 
 require_once '../src/SemanticScuttle/header.php';
 
 /* Service creation: only useful services are created */
-$b2tservice       = SemanticScuttle_Service_Factory :: get('Bookmark2Tag');
-$cdservice        = SemanticScuttle_Service_Factory :: get('CommonDescription');
+$b2tservice = SemanticScuttle_Service_Factory::get('Bookmark2Tag');
+$cdservice  = SemanticScuttle_Service_Factory::get('CommonDescription');
 
 /* Managing all possible inputs */
-isset($_POST['confirm']) ? define('POST_CONFIRM', $_POST['confirm']): define('POST_CONFIRM', '');
-isset($_POST['cancel']) ? define('POST_CANCEL', $_POST['cancel']): define('POST_CANCEL', '');
-isset($_POST['description']) ? define('POST_DESCRIPTION', $_POST['description']): define('POST_DESCRIPTION', '');
-isset($_POST['referrer']) ? define('POST_REFERRER', $_POST['referrer']): define('POST_REFERRER', '');
+isset($_POST['confirm'])
+    ? define('POST_CONFIRM', $_POST['confirm'])
+    : define('POST_CONFIRM', '');
+isset($_POST['cancel'])
+    ? define('POST_CANCEL', $_POST['cancel'])
+    : define('POST_CANCEL', '');
+isset($_POST['description'])
+    ? define('POST_DESCRIPTION', $_POST['description'])
+    : define('POST_DESCRIPTION', '');
+isset($_POST['referrer'])
+    ? define('POST_REFERRER', $_POST['referrer'])
+    : define('POST_REFERRER', '');
 
 
 /* Managing current logged user */
@@ -39,33 +43,36 @@ $currentUser = $userservice->getCurrentObjectUser();
 list ($url, $tag) = explode('/', $_SERVER['PATH_INFO']);
 
 //permissions
-if(!$userservice->isLoggedOn() || (!$GLOBALS['enableCommonTagDescriptionEditedByAll'] && !$currentUser->isAdmin())) {
-	$tplVars['error'] = T_('Permission denied.');
-	$templateservice->loadTemplate('error.500.tpl', $tplVars);
-	exit();
+if (!$userservice->isLoggedOn()
+    || (!$GLOBALS['enableCommonTagDescriptionEditedByAll']
+        && !$currentUser->isAdmin()
+    )
+) {
+    $tplVars['error'] = T_('Permission denied.');
+    $templateservice->loadTemplate('error.500.tpl', $tplVars);
+    exit();
 }
 
-$template   = 'tagcommondescriptionedit.tpl';
+$template = 'tagcommondescriptionedit.tpl';
 
 if (POST_CONFIRM) {
-
-	if ( strlen($tag)>0 &&
-	$cdservice->addTagDescription($tag, stripslashes(POST_DESCRIPTION), $currentUser->getId(), time())
-	) {
-		$tplVars['msg'] = T_('Tag common description updated');
-		header('Location: '. POST_REFERRER);
-	} else {
-		$tplVars['error'] = T_('Failed to update the tag common description');
-		$template         = 'error.500.tpl';
-	}
-} elseif (POST_CANCEL) {
-	header('Location: '. POST_REFERRER);
+    if (strlen($tag) > 0
+        && $cdservice->addTagDescription($tag, stripslashes(POST_DESCRIPTION), $currentUser->getId(), time())
+    ) {
+        $tplVars['msg'] = T_('Tag common description updated');
+        header('Location: '. POST_REFERRER);
+    } else {
+        $tplVars['error'] = T_('Failed to update the tag common description');
+        $template         = 'error.500.tpl';
+    }
+} else if (POST_CANCEL) {
+    header('Location: '. POST_REFERRER);
 } else {
-	$tplVars['subtitle']    = T_('Edit Tag Common Description') .': '. $tag;
-	$tplVars['formaction']  = $_SERVER['SCRIPT_NAME'] .'/'. $tag;
-	$tplVars['referrer']    = $_SERVER['HTTP_REFERER'];
-	$tplVars['tag']         = $tag;
-	$tplVars['description'] = $cdservice->getLastTagDescription($tag);
+    $tplVars['subtitle']    = T_('Edit Tag Common Description') .': '. $tag;
+    $tplVars['formaction']  = $_SERVER['SCRIPT_NAME'] .'/'. $tag;
+    $tplVars['referrer']    = $_SERVER['HTTP_REFERER'];
+    $tplVars['tag']         = $tag;
+    $tplVars['description'] = $cdservice->getLastTagDescription($tag);
 }
 $templateservice->loadTemplate($template, $tplVars);
 ?>
