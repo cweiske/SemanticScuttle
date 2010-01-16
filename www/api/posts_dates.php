81c10ea65b8b0afac6668535451009917c61463a
cweiske <cweiske@b3834d28-1941-0410-a4f8-b48e95affb8f>
2010-01-16 10:03:30 +0000

    Coding standards changes, add header docblock
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@583 b3834d28-1941-0410-a4f8-b48e95affb8f

--- www/api/posts_dates.php
+++ www/api/posts_dates.php
@@ -1,37 +1,53 @@
 <?php
-// Implements the del.icio.us API request for a user's post counts by date (and optionally
-// by tag).
+/**
+ * Implements the del.icio.us API request for a user's post counts by date
+ * (and optionally by tag).
+ *
+ * SemanticScuttle - your social bookmark manager.
+ *
+ * PHP version 5.
+ *
+ * @category Bookmarking
+ * @package  SemanticScuttle
+ * @author   Benjamin Huynh-Kim-Bang <mensonge@users.sourceforge.net>
+ * @author   Christian Weiske <cweiske@cweiske.de>
+ * @author   Eric Dane <ericdane@users.sourceforge.net>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     http://sourceforge.net/projects/semanticscuttle
+ */
 
 // Force HTTP authentication first!
-require_once('httpauth.inc.php');
-require_once '../../src/SemanticScuttle/header.php';
+require_once 'httpauth.inc.php';
 
 /* Service creation: only useful services are created */
-$bookmarkservice =SemanticScuttle_Service_Factory::get('Bookmark');
+$bookmarkservice = SemanticScuttle_Service_Factory::get('Bookmark');
 
 
 // Check to see if a tag was specified.
-if (isset($_REQUEST['tag']) && (trim($_REQUEST['tag']) != ''))
+if (isset($_REQUEST['tag']) && (trim($_REQUEST['tag']) != '')) {
     $tag = trim($_REQUEST['tag']);
-else
-    $tag = NULL;
+} else {
+    $tag = null;
+}
 
 // Get the posts relevant to the passed-in variables.
-$bookmarks =& $bookmarkservice->getBookmarks(0, NULL, $userservice->getCurrentUserId(), $tag);
+$bookmarks = $bookmarkservice->getBookmarks(
+    0, null, $userservice->getCurrentUserId(), $tag
+);
 
 //	Set up the XML file and output all the tags.
 header('Content-Type: text/xml');
 echo '<?xml version="1.0" standalone="yes" ?'.">\r\n";
 echo '<dates tag="'. (is_null($tag) ? '' : filter($tag, 'xml')) .'" user="'. filter($currentUser->getUsername(), 'xml') ."\">\r\n";
 
-$lastdate = NULL;
-foreach($bookmarks['bookmarks'] as $row) {
+$lastdate = null;
+foreach ($bookmarks['bookmarks'] as $row) {
     $thisdate = gmdate('Y-m-d', strtotime($row['bDatetime']));
-    if ($thisdate != $lastdate && $lastdate != NULL) {
+    if ($thisdate != $lastdate && $lastdate != null) {
         echo "\t<date count=\"". $count .'" date="'. $lastdate ."\" />\r\n";
         $count = 1;
     } else {
-        $count = $count + 1;
+        ++$count;
     }
     $lastdate = $thisdate;
 }
