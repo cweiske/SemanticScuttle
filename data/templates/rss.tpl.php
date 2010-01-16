9bcab8c363da59f61086ec6ac9b7d77a75c5c8a4
cweiske <cweiske@b3834d28-1941-0410-a4f8-b48e95affb8f>
2010-01-16 10:13:52 +0000

    lowercase utf-8 because i like that better
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@590 b3834d28-1941-0410-a4f8-b48e95affb8f

--- data/templates/rss.tpl.php
+++ data/templates/rss.tpl.php
@@ -1,5 +1,5 @@
 <?php
-echo '<' . '?xml version="1.0" encoding="UTF-8" ?' . ">\n";
+echo '<' . '?xml version="1.0" encoding="utf-8" ?' . ">\n";
 ?>
 <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
 <channel>
