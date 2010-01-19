777f7f072c92bb2db3db393a7607fb68b5e5b56a
cweiske <cweiske@b3834d28-1941-0410-a4f8-b48e95affb8f>
2010-01-19 20:32:10 +0000

    Fix bug #2934891: RSS XML was sometimes invalid because  special characters did not get escaped.
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@617 b3834d28-1941-0410-a4f8-b48e95affb8f

--- data/templates/rss.tpl.php
+++ data/templates/rss.tpl.php
@@ -3,22 +3,22 @@ echo '<' . '?xml version="1.0" encoding="utf-8" ?' . ">\n";
 ?>
 <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
 <channel>
-    <title><?php echo $feedtitle; ?></title> 
-    <link><?php echo $feedlink; ?></link> 
-    <description><?php echo $feeddescription; ?></description>
+    <title><?php echo htmlspecialchars($feedtitle); ?></title>
+    <link><?php echo htmlspecialchars($feedlink); ?></link>
+    <description><?php echo htmlspecialchars($feeddescription); ?></description>
     <pubDate><?php echo date('r'); ?></pubDate>
     <lastBuildDate><?php echo $feedlastupdate ?></lastBuildDate>
     <ttl>60</ttl>
 
 <?php foreach($bookmarks as $bookmark): ?>
     <item>
-        <title><?php echo $bookmark['title']; ?></title>
-        <link><?php echo $bookmark['link']; ?></link>
-        <description><?php echo $bookmark['description']; ?></description>
-        <dc:creator><?php echo $bookmark['creator']; ?></dc:creator>
+        <title><?php echo htmlspecialchars($bookmark['title']); ?></title>
+        <link><?php echo htmlspecialchars($bookmark['link']); ?></link>
+        <description><?php echo htmlspecialchars($bookmark['description']); ?></description>
+        <dc:creator><?php echo htmlspecialchars($bookmark['creator']); ?></dc:creator>
         <pubDate><?php echo $bookmark['pubdate']; ?></pubDate>
 <?php foreach($bookmark['tags'] as $tag): ?>
-        <category><?php echo $tag; ?></category>
+        <category><?php echo htmlspecialchars($tag); ?></category>
 <?php endforeach; ?>
     </item>
 <?php endforeach; ?>
