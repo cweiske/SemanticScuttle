7a8c92ff1aaa3c4b6b60319a9502288fc9395531
cweiske <cweiske@b3834d28-1941-0410-a4f8-b48e95affb8f>
2010-02-09 15:55:01 +0000

    prepare 0.96.1
    git-svn-id: https://semanticscuttle.svn.sourceforge.net/svnroot/semanticscuttle/trunk@650 b3834d28-1941-0410-a4f8-b48e95affb8f

--- data/templates/about.tpl.php
+++ data/templates/about.tpl.php
@@ -17,7 +17,7 @@ $this->includeTemplate($GLOBALS['top_include']);
 
 
 <?php if(!is_null($currentUser) && $currentUser->isAdmin()): ?>
-<li>SemanticScuttle v0.96.0</li>
+<li>SemanticScuttle v0.96.1</li>
 <?php endif ?>
 </ul>
 
