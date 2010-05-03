087189a503cb26dab7e0aaaf1d307c6c21f2107e
Christian Weiske <cweiske@cweiske.de>
2010-05-03 22:55:53 +0200

    first work on quickform2 registration form

--- data/templates/register.tpl.php
+++ data/templates/register.tpl.php
@@ -10,35 +10,36 @@ window.onload = function() {
 
 <p><?php echo sprintf(T_('Sign up here to create a free %s account. All the information requested below is required'), $GLOBALS['sitename']); ?>.</p>
 
-<form action="<?php echo $formaction; ?>" method="post">
+<form<?php echo $form['attributes']; ?>>
+<?php echo implode('', $form['hidden']); ?>
 <table>
 <tr>
-    <th align="left"><label for="username"><?php echo T_('Username'); ?></label></th>
-    <td><input type="text" id="username" name="username" size="20" class="required" onkeyup="isAvailable(this, '')" /> </td>
+    <th align="left"><?php echo $form['username']['labelhtml']; ?></th>
+    <td><?php echo $form['username']['html']; ?></td>
     <td id="availability"><?php echo '←'.T_(' at least 5 characters, alphanumeric (no spaces, no dots or other special ones)') ?></td>
 </tr>
 <tr>
-    <th align="left"><label for="password"><?php echo T_('Password'); ?></label></th>
-    <td><input type="password" id="password" name="password" size="20" class="required" /></td>
+    <th align="left"><?php echo $form['password']['labelhtml']; ?></th>
+    <td><?php echo $form['password']['html']; ?></td>
     <td></td>
 </tr>
 <tr>
-    <th align="left"><label for="email"><?php echo T_('E-mail'); ?></label></th>
-    <td><input type="text" id="email" name="email" size="40" class="required" /></td>
+    <th align="left"><?php echo $form['email']['labelhtml']; ?></th>
+    <td><?php echo $form['email']['html']; ?></td>
     <td><?php echo '←'.T_(' to send you your password if you forget it')?></td>
 </tr>
 
-<?php if(strlen($antispamQuestion)>0) {?>
+<?php if (isset($form['antispamAnswer'])) {?>
 <tr>
-    <th align="left"><label for="antispamAnswer"><?php echo T_('Antispam question'); ?></label></th>
-    <td><input type="text" id="antispamAnswer" name="antispamAnswer" size="40" class="required" value="<?php echo $antispamQuestion; ?>" onfocus="if (this.value == '<?php echo $antispamQuestion; ?>') this.value = '';" onblur="if (this.value == '') this.value = '<?php echo $antispamQuestion; ?>';"/></td>
+    <th align="left"><?php echo $form['antispamAnswer']['labelhtml']; ?></label></th>
+    <td><?php echo $form['antispamAnswer']['html']; ?></td>
     <td></td>
 </tr>
 <?php } ?>
 
 <tr>
     <td></td>
-    <td><input type="submit" name="submitted" value="<?php echo T_('Register'); ?>" /></td>
+    <td><?php echo $form['submit']['html']; ?></td>
     <td></td>
 </tr>
 </table>
