9c412f89ff7187bef1b2fbf0bc1dfd6b8f3b6aed
Christian Weiske <cweiske@cweiske.de>
2010-11-02 19:09:27 +0100

    we do not need the entities anymore since we will be using the characters themselves now

--- data/templates/top.inc.php
+++ data/templates/top.inc.php
@@ -1,12 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd" [
-  <!ENTITY % xhtml-symbol
-    PUBLIC "-//W3C//ENTITIES Symbols for XHTML//EN"
-    "http://www.w3.org/MarkUp/DTD/xhtml-symbol.ent">
-  <!-- firefox bitches on that reference above and does not load it -->
-  <!ENTITY larr     "&#8592;" ><!-- leftwards arrow, U+2190 ISOnum -->
-%xhtml-symbol;
-]>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
  <head>
   <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
