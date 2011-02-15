128eb204081787a10b06d6962ad9e49bb2481399
Christian Weiske <cweiske@cweiske.de>
2011-02-16 00:16:33 +0100

    prepare release of 0.97.2 by updating versions and setting the release date

--- data/templates/about.tpl.php
+++ data/templates/about.tpl.php
@@ -17,7 +17,7 @@ $this->includeTemplate($GLOBALS['top_include']);
 
 
 <?php if(!is_null($currentUser) && $currentUser->isAdmin()): ?>
-<li>SemanticScuttle v0.97.0</li>
+<li>SemanticScuttle v0.97.2</li>
 <?php endif ?>
 </ul>
 
