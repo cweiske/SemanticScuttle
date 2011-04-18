87eab925e45dd72e18c097329d5b751fd48ceabb
Christian Weiske <cweiske@cweiske.de>
2011-04-18 19:16:58 +0200

    make edit test much easier and do not rely on manual cookie setting

--- tests/Api/PostsAddTest.php
+++ tests/Api/PostsAddTest.php
@@ -475,31 +475,31 @@ TXT;
         $this->setUnittestConfig(
             array('defaults' => array('privacy' => 2))
         );
-        list($req, $uId) = $this->getAuthRequest('?unittestMode=1');
+
+        list($req, $uId) = $this->getLoggedInRequest('?unittestMode=1');
+        $cookies = $req->getCookieJar();
         $req->setMethod(HTTP_Request2::METHOD_POST);
         $req->addPostParameter('url', 'http://www.example.org/testdefaultprivacyposts_edit');
         $req->addPostParameter('description', 'Test bookmark 2 for default privacy.');
         $req->addPostParameter('status', '0');
-        $req->send();
+        $res = $req->send();
+        $this->assertEquals(
+            200, $res->getStatus(),
+            'Adding bookmark failed: ' . $res->getBody());
         $bms = $this->bs->getBookmarks(0, null, $uId);
-        $bm = reset($bms['bookmarks']);
+        $bm  = reset($bms['bookmarks']);
         $bmId = $bm['bId'];
-        $oldUid = $uId;
+
         $reqUrl = $GLOBALS['unittestUrl'] . 'edit.php/' . $bmId . '?unittestMode=1'; 
-        list($req, $uId) = $this->getAuthRequest('?unittestMode=1');
-        $req->setMethod(HTTP_Request2::METHOD_POST);
-        $req->setUrl($reqUrl);
-        $testcookiekey = md5($GLOBALS['dbname'].$GLOBALS['tableprefix']).'-login';
-        $userinfo = $this->us->getUser($oldUid);
-        $testcookiepassword = $userinfo['password'];
-        $testusername = $userinfo['username'];
-        $testcookievalue = $oldUid . ':' . md5($testusername . $testcookiepassword);
-        $req->setCookieJar(true);
-        $req->addCookie($testcookiekey, $testcookievalue);
-        $req->addPostParameter('address', 'http://www.example.org/testdefaultprivacyposts_edit');
-        $req->addPostParameter('title', 'Test bookmark 2 for default privacy.');
-        $req->addPostParameter('submitted', '1');
-        $req->send();
+        $req2 = new HTTP_Request2($reqUrl, HTTP_Request2::METHOD_POST);
+        $req2->setCookieJar($cookies);
+        $req2->addPostParameter('address', 'http://www.example.org/testdefaultprivacyposts_edit');
+        $req2->addPostParameter('title', 'Test bookmark 2 for default privacy.');
+        $req2->addPostParameter('submitted', '1');
+        $res = $req2->send();
+
+        $this->assertEquals(302, $res->getStatus(), 'Editing bookmark failed');
+
         $bm = $this->bs->getBookmark($bmId);
         $this->assertEquals('2', $bm['bStatus']);
     }//end testDefaultPrivacyEdit 
