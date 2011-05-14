01f21995dd5d488fa13be54049610bec919ced95
Christian Weiske <cweiske@cweiske.de>
2011-05-14 10:30:01 +0200

    fix google custom search xml

--- www/gsearch/context.php
+++ www/gsearch/context.php
@@ -1,16 +1,35 @@
 <?php
+/**
+ * Google custom search context information for SemanticScuttle.
+ * Tells Google meta data about the search.
+ *
+ * SemanticScuttle - your social bookmark manager.
+ *
+ * PHP version 5.
+ *
+ * @category Bookmarking
+ * @package  SemanticScuttle
+ * @author   Benjamin Huynh-Kim-Bang <mensonge@users.sourceforge.net>
+ * @author   Christian Weiske <cweiske@cweiske.de>
+ * @author   Eric Dane <ericdane@users.sourceforge.net>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     http://sourceforge.net/projects/semanticscuttle
+ * @link     http://www.google.com/cse/docs/cref.html
+ */
 require_once '../www-header.php';
 
-if($GLOBALS['enableGoogleCustomSearch'] == false) {
-    echo "Google Custom Search disabled. You can enable it into the config.php file.";
-    die;
+if ($GLOBALS['enableGoogleCustomSearch'] == false) {
+    header('HTTP/1.0 403 Forbidden');
+    header('Content-Type: text/plain; charset=utf-8');
+    echo "Google Custom Search disabled."
+        . " You can enable it into the config.php file.\n";
+    die();
 }
 ?>
-
-<!--?xml version="1.0" encoding="UTF-8" ?-->
+<?xml version="1.0" encoding="UTF-8" ?>
 <GoogleCustomizations>
     <CustomSearchEngine>
-        <Title><?php echo $GLOBALS['sitename'] ?></Title>
+        <Title><?php echo htmlspecialchars($GLOBALS['sitename']) ?></Title>
         <Description><?php echo filter($GLOBALS['welcomeMessage']) ?></Description>
         <Context>
            <BackgroundLabels>
