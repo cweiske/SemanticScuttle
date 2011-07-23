dc69d1ad13889022b90c24b6ccbff441f52403cd
Christian Weiske <cweiske@cweiske.de>
2011-07-23 08:27:17 +0200

    Fix bug #3375635: XML parsing problem in top.inc.php (happened only when short_open_tags were activated)

--- data/templates/default/top.inc.php
+++ data/templates/default/top.inc.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?php echo '<'; ?>?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
  <head>
