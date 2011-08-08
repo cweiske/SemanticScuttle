08435a517af7f1ec73baed6d7387b1b9611e3379
Christian Weiske <cweiske@cweiske.de>
2011-08-08 07:30:59 +0200

    prepare 0.98.2

--- data/templates/default/about.tpl.php
+++ data/templates/default/about.tpl.php
@@ -17,7 +17,7 @@ $this->includeTemplate($GLOBALS['top_include']);
 
 
 <?php if(!is_null($currentUser) && $currentUser->isAdmin()): ?>
-<li>SemanticScuttle v0.98.1</li>
+<li>SemanticScuttle v0.98.2</li>
 <?php endif ?>
 </ul>
 
