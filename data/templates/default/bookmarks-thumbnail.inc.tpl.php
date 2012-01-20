645f21b1a90b7369218913eea8fba711e12f9d20
Christian Weiske <cweiske@cweiske.de>
2012-01-20 15:20:16 +0100

    Fix bug: Invalid HTML when website thumbnails are activated

--- data/templates/default/bookmarks-thumbnail.inc.tpl.php
+++ data/templates/default/bookmarks-thumbnail.inc.tpl.php
@@ -13,6 +13,6 @@ $thumbnailHash = md5(
     $address . $GLOBALS['thumbnailsUserId'] . $GLOBALS['thumbnailsKey']
 );
 //echo '<a href="'. $address .'"'. $rel .' ><img class="thumbnail" src="http://www.artviper.net/screenshots/screener.php?url='.$address.'&w=120&sdx=1280&userID='.$GLOBALS['thumbnailsUserId'].'&hash='.$thumbnailHash.'" />';
-echo '<img class="thumbnail" onclick="window.location.href=\''.$address.'\'" src="http://www.artviper.net/screenshots/screener.php?url='.$address.'&w=120&sdx=1280&userID='.$GLOBALS['thumbnailsUserId'].'&hash='.$thumbnailHash.'" />';
+echo '<img class="thumbnail" onclick="window.location.href=\''.htmlspecialchars($address).'\'" src="http://www.artviper.net/screenshots/screener.php?url='.htmlspecialchars($address).'&amp;w=120&amp;sdx=1280&amp;userID='.$GLOBALS['thumbnailsUserId'].'&amp;hash='.$thumbnailHash.'" />';
 
 ?>
